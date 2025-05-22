0845677fade9cf60f10cb2bb413ee8c061e13dd8
Haseena Rahaman <haseenar2104@gmail.com>
2025-05-22 13:58:42 +0530

    Add files via upload

---
+++ file2.js
@@ -0,0 +1,20 @@
+let toggle=document.querySelector("#togg");
+let body=document.querySelector("body");
+let currMode = "light";
+
+toggle.addEventListener("click",()=>{
+    if(currMode === "light"){
+        currMode ="dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    }
+    else
+    {
+        currMode="light";
+        body.classList.add("light")
+        body.classList.remove("dark");
+
+    }
+    console.log(currMode);
+    
+});
